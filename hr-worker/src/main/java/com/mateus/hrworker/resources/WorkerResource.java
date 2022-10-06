d1ca37d2fe0a994292b2147fa11b564df8dcb54c
Mateus Brandao Ramos Carneiro <mateusbrandao@id.uff.br>
2022-10-05 21:16:22 -0300

    Configuracao do hr-worker como um config client

--- hr-worker/src/main/java/com/mateus/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/mateus/hrworker/resources/WorkerResource.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,9 +19,20 @@ import com.mateus.hrworker.repositories.WorkerRepository;
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 	
-
+	private static Logger log = LoggerFactory.getLogger(WorkerResource.class);
+	
+	@Value("${test.config}")
+	private String testConfig;
+	
 	@Autowired
 	private WorkerRepository repository;
+	
+	@GetMapping(value = "/configs")
+	public ResponseEntity<Void> getConfigs(){
+		log.info("Config = "+testConfig);
+		List<Worker> list = repository.findAll();
+		return ResponseEntity.noContent().build();
+	}
 	
 	@GetMapping
 	public ResponseEntity<List<Worker>> findAll(){
