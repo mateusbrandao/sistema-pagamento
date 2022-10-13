7628214099353ba035734e4e286e60a32a914162
Mateus Brandao Ramos Carneiro <mateusbrandao@id.uff.br>
2022-10-13 17:41:16 -0300

    Configuracao do hr-oauth para que ele possa gerar o JWT e possa funcionar como servidor de autorizacao

--- hr-oauth/src/main/java/com/mateus/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/mateus/hroauth/services/UserService.java
@@ -3,13 +3,16 @@ package com.mateus.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.mateus.hroauth.entities.User;
 import com.mateus.hroauth.feignclients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService{
 	
 	private static Logger log = LoggerFactory.getLogger(UserService.class);
 
@@ -23,6 +26,17 @@ public class UserService {
 			throw new IllegalArgumentException("Email nao encontrado");
 		}
 		log.info("Email encontrado: "+email);
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClient.findByEmal(username).getBody();
+		if(user == null) {
+			log.error("Email nao encontrado: "+username);
+			throw new UsernameNotFoundException("Email nao encontrado");
+		}
+		log.info("Email encontrado: "+username);
 		return user;
 	}
 	
